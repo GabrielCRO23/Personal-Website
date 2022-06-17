ab0a9f27e1632fc8c6542af81025bcdeb8ff0059
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-06-17 05:57:02 -0400

    Added appheader bar, added additional theme, added font size responsiveness

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+
 import './App.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { green, purple } from '@mui/material/colors';
+import { ThemeProvider, createTheme, responsiveFontSizes} from '@mui/material/styles';
+
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
-import ButtonGroup from '@mui/material/ButtonGroup';
+
 import ArticleIcon from '@mui/icons-material/Article';
 import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { styled } from '@mui/system';
 
 
+
+const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 
 const theme = createTheme({
@@ -23,20 +29,52 @@ const theme = createTheme({
   },
 });
 
-
-
-
+let theme2 = createTheme();
+theme2 = responsiveFontSizes(theme2);
 
 
 
 
 
 function App() {
+  const [chosen, setChosen] = useState();
+  //const [selectedButton, setSelectedButton] = useState();
+  const buttons = [
+    { text: 'About', id: 'abc123' },
+    { text: 'Projects', id: 'def456' },
+    { text: 'Contact', id: 'ghi789' },
+  ];
+
   return (
+   
+
+    
     <Container maxWidth="lg">
+    <ThemeProvider theme={theme2}>
     <ThemeProvider theme={theme}>
+    
     <div className="App">
-      
+
+    <AppBar position="fixed" sx={{backgroundColor: "#e1e0dd", display: 'flex', flexDirection: 'row', justifyContent: { md: "flex-end", xs: "center"} }}>
+        <Toolbar variant="dense">
+
+      {
+                  buttons.map(button => (
+                    <Button
+                      href={"#" + button.text.toLowerCase()}
+                      onClick={() => setChosen(button.id)}
+                      sx= {{ backgroundColor: button.id === chosen? 'white' : 'primary', color: button.id === chosen? 'primary' : 'primary', cursor: 'pinter',}}
+                      key={button.id}
+                    >
+                      {button.text}
+                    </Button>
+                  ))
+                }
+
+
+        </Toolbar>
+      </AppBar>
+      <Offset />
       <header className="App-header">
       <Box sx={{
         display: 'flex',
@@ -48,13 +86,7 @@ function App() {
         display: 'flex',
         justifyContent: 'space-between'
       }}>
-      <Typography variant="h3" color="secondary" align="left">Software Developer</Typography>
-      <ArticleIcon color="secondary" fontSize="large" label="hi"sx={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        position: 'relative',
-
-      }}></ArticleIcon>
+      <Typography variant="h3" color="secondary" align="left">Programmer</Typography>
       
       </Box>
       
@@ -62,9 +94,15 @@ function App() {
       <Box sx={{
         display: 'flex',
       }}>
-      <Button color="primary" >About</Button>
-      <Button color="primary" >Projects</Button>
-      <Button color="primary" >Contact</Button>
+                
+
+
+
+
+
+
+
+
       </Box>
       
       </Box>
@@ -72,16 +110,19 @@ function App() {
       
       <hr></hr>
       
-      <Typography variant="body1" color="primary" paragraph={true} align="left">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, fugiat omnis enim tempora necessitatibus sapiente iste non corporis temporibus, earum esse optio ullam obcaecati quam, mollitia minus suscipit. Quidem facilis perferendis vel eaque, nostrum illo, nulla distinctio iusto cupiditate maxime molestiae debitis dignissimos numquam. Pariatur, reiciendis fugiat sed quidem vitae laudantium tenetur nobis commodi est, voluptas autem eum distinctio? Obcaecati sit labore perspiciatis deserunt. Molestias, omnis quo. Quo quasi placeat iure. Quam, qui. Nobis sapiente odit neque atque. Dolorem, quia numquam exercitationem, tenetur recusandae repellat autem in fugit vero id, consequuntur consequatur sequi dolores. Assumenda sed fuga nostrum in laborum!
+      <Box>
+      <Typography sx={{}}variant="body1" color="primary" paragraph={true} align="left">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, fugiat omnis enim tempora necessitatibus sapiente iste non corporis temporibus, earum esse optio ullam obcaecati quam, mollitia minus suscipit. Quidem facilis perferendis vel eaque, nostrum illo, nulla distinctio iusto cupiditate maxime molestiae debitis dignissimos numquam. Pariatur, reiciendis fugiat sed quidem vitae laudantium tenetur nobis commodi est, voluptas autem eum distinctio? Obcaecati sit labore perspiciatis deserunt. Molestias, omnis quo. Quo quasi placeat iure. Quam, qui. Nobis sapiente odit neque atque. Dolorem, quia numquam exercitationem, tenetur recusandae repellat autem in fugit vero id, consequuntur consequatur sequi dolores. Assumenda sed fuga nostrum in laborum!
       </Typography>
       <Typography variant="body1" color="primary" paragraph={true} align="left">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, fugiat omnis enim tempora necessitatibus sapiente iste non corporis temporibus, earum esse optio ullam obcaecati quam, mollitia minus suscipit. Quidem facilis perferendis vel eaque, nostrum illo, nulla distinctio iusto cupiditate maxime molestiae debitis dignissimos numquam. Pariatur, reiciendis fugiat sed quidem vitae laudantium tenetur nobis commodi est, voluptas autem eum distinctio? Obcaecati sit labore perspiciatis deserunt. Molestias, omnis quo. Quo quasi placeat iure. Quam, qui. Nobis sapiente odit neque atque. Dolorem, quia numquam exercitationem, tenetur recusandae repellat autem in fugit vero id, consequuntur consequatur sequi dolores. Assumenda sed fuga nostrum in laborum!
       </Typography>
-        
+      </Box>
       </header>
     </div>
+    </ThemeProvider>
     </ThemeProvider>
     </Container>
   );
 }
 
 export default App;
+
